e5cec7d0a2e2622bb9387d66d99a15567529a333
roman <rgnevashev@gmail.com>
2016-02-04 10:02:32 +0300

    update

--- package.js
+++ package.js
@@ -1,15 +1,15 @@
 Package.describe({
   name: 'rgnevashev:angular-bootstrap-slider',
-  version: '0.1.7',
+  version: '0.1.24',
   summary: 'An angular directive for seiyria-bootstrap-slider',
   git: 'https://github.com/seiyria/angular-bootstrap-slider',
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.2.1');
-  api.use(['rgnevashev:bootstrap-slider@6.0.0']);
+  //api.versionsFrom('1.2.1');
+  api.use(['rgnevashev:bootstrap-slider@6.0.14']);
   api.addFiles([
     "slider.js"
   ], 'client');
-}); 
+});
